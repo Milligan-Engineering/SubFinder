260f137885d5b7f1f76ef3be9a6dc3c25994af6c
Amperryman <Amperryman@my.milligan.edu>
2020-01-27 10:05:38 -0500

    Add project files.

---
+++ SubFinder/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string name, date, subject, location;
+int main()
+{
+	cout << "welcome teacher\n";
+	cout << "Enter your name...";
+	cin >> name;
+	cout << name;
+	return 0;
+}
