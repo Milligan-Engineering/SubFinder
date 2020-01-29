07a17b01e55f17fa262ea861016c5191e5c12453
Amperryman <Amperryman@my.milligan.edu>
2020-01-29 09:52:59 -0500

    first version of subfinder

--- SubFinder/Source.cpp
+++ SubFinder/Source.cpp
@@ -2,12 +2,22 @@
 #include <string>
 using namespace std;
 
-string name, date, subject, location;
+int date, room_number;
+string name, month, subject, school_location;
 int main()
 {
 	cout << "welcome teacher\n";
 	cout << "Enter your name...";
 	cin >> name;
-	cout << name;
+	cout << "Enter month of substition(i.e. Feduary)...";
+	cin >> month;
+	cout << "Enter day of the substition(i.e. 12)...";
+	cin >> date;
+	cout << "Enter the subject...";
+	cin >> subject;
+	cout << "Enter school location(NGS)...";
+	cin >> school_location;
+	cout << "Enter room number...";
+	cin >> room_number;
 	return 0;
 }
