6058a046dfb84820bec77632b5da7b2d6bdfb103
Amperryman <Amperryman@my.milligan.edu>
2020-02-04 22:13:31 -0500

    changed the file name

--- SubFinder/SubFinder.cpp
+++ SubFinder/SubFinder.cpp
@@ -3,14 +3,14 @@
 // Email Address: Amperryman@my.milligan.edu
 // Assignment Number: PM03
 // Description: Program to conect teachers and substitues 
-// Last changed: 02/03/20
+// Last changed: 02/04/20
 
 #include <iostream>
 #include <string>
 using namespace std;
 
 int  date, room_number, number_students, age, x;
-string  name, month, subject, school_location, email;
+string  nameTeacher, month, subjectTeacher, school_location, emailTeacher, nameSub, subjectSub, emailSub;
 
 int main()
 {
@@ -21,29 +21,36 @@ int main()
 		cout << "welcome teacher\n";
 		cout << "note: when entering in your information please use no spaces and no capital letters\n";
 		cout << "Enter your name...";
-		cin >> name;
+		cin >> nameTeacher;
 		cout << "Enter your email address...";
-		cin >> email;
+		cin >> emailTeacher;
 		cout << "Enter month of substition(i.e. Feduary)...";
 		cin >> month;
 		cout << "Enter day of the substition(i.e. 12)...";
 		cin >> date;
 		cout << "Enter the subject...";
-		cin >> subject;
+		cin >> subjectTeacher;
 		cout << "Enter school location(i.e. NGS)...";
 		cin >> school_location;
 		cout << "Enter room number...";
 		cin >> room_number;
-		cout << "Enter how many students you will have during the day...";
+		cout << "Enter the average number of studnts you have during a day...";//you are supposed to enter the average amout of studenst you have in a single class period
 		cin >> number_students;
 		cout << "Enter how old are a majority of the students...";
 		cin >> age;
-		cout << "The assumed level of difficulty for subing" << " " << name << " " << "class is... ";
+		cout << "The assumed level of difficulty for subing" << " " << nameTeacher << " " << "class is... ";
 		cout << (number_students * age);
 	}
 	else
 	{
 		cout << "welcome substitute\n";
+		cout << "note: when entering in your information please use no spaces and no capital letters\n";
+		cout << "Enter your name...";
+		cin >> nameSub;
+		cout << "Enter your preferred subject...";
+		cin >> subjectSub;
+		cout << "Enter email...";
+		cin >> emailSub;
 	}
 	return 0;
 }
