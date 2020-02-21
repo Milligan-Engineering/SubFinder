eee54df1d6d9b4e100014fadc4ac6a02f44c99f6
Amperryman <Amperryman@my.milligan.edu>
2020-02-21 10:09:04 -0500

    added do while to all the switches

--- SubFinder/SubFinder.cpp
+++ SubFinder/SubFinder.cpp
@@ -38,126 +38,138 @@ int main()
 			cin >> nameTeacher;
 			cout << "Enter your email address...";
 			cin >> emailTeacher;
-			cout << "Select month of substition\n";
-			cout << "Choose 1 for JANUARY.\n"
-				<< "Choose 2 for FEBRUARY.\n"
-				<< "Choose 3 for MARCH.\n"
-				<< "Choose 4 for APRIL.\n"
-				<< "Choose 5 for MAY.\n"
-				<< "Choose 6 for JUNE.\n"
-				<< "Choose 7 for JULY.\n"
-				<< "Choose 8 for AUGUST.\n"
-				<< "Choose 9 for SEPTEMBER.\n"
-				<< "Choose 10 for OCTOBER.\n"
-				<< "Choose 11 for NOVEMBER.\n"
-				<< "Choose 12 for DECEMBER.\n";
-			cin >> choiceMonth;
-			switch (choiceMonth)
+			do
 			{
-			case 1:
-				month = "January";
-				break;
-			case 2:
-				month = "February";
-				break;
-			case 3:
-				month = "March";
-				break;
-			case 4:
-				month = "April";
-				break;
-			case 5:
-				month = "May";
-				break;
-			case 6:
-				month = "June";
-				break;
-			case 7:
-				month = "July";
-				break;
-			case 8:
-				month = "August";
-				break;
-			case 9:
-				month = "September";
-				break;
-			case 10:
-				month = "October";
-				break;
-			case 11:
-				month = "November";
-				break;
-			case 12:
-				month = "December";
-				break;
-			}
+				cout << "Select month of substition\n";
+				cout << "Choose 1 for JANUARY.\n"
+					<< "Choose 2 for FEBRUARY.\n"
+					<< "Choose 3 for MARCH.\n"
+					<< "Choose 4 for APRIL.\n"
+					<< "Choose 5 for MAY.\n"
+					<< "Choose 6 for JUNE.\n"
+					<< "Choose 7 for JULY.\n"
+					<< "Choose 8 for AUGUST.\n"
+					<< "Choose 9 for SEPTEMBER.\n"
+					<< "Choose 10 for OCTOBER.\n"
+					<< "Choose 11 for NOVEMBER.\n"
+					<< "Choose 12 for DECEMBER.\n";
+				cin >> choiceMonth;
+				switch (choiceMonth)
+				{
+				case 1:
+					month = "January";
+					break;
+				case 2:
+					month = "February";
+					break;
+				case 3:
+					month = "March";
+					break;
+				case 4:
+					month = "April";
+					break;
+				case 5:
+					month = "May";
+					break;
+				case 6:
+					month = "June";
+					break;
+				case 7:
+					month = "July";
+					break;
+				case 8:
+					month = "August";
+					break;
+				case 9:
+					month = "September";
+					break;
+				case 10:
+					month = "October";
+					break;
+				case 11:
+					month = "November";
+					break;
+				case 12:
+					month = "December";
+					break;
+				}
+			} while (choiceMonth > 12);
 			cout << "Enter day of the substition(i.e. 12)...";
 			cin >> date;
-			cout << "Select the subject\n";
-			cout << "Choose 1 to select MATH. \n"
-				<< "Choose 2 to select ENGLISH. \n"
-				<< "Choose 3 to select HISTORY. \n"
-				<< "Choose 4 to selcet SCIENCE. \n"
-				<< "Choose 5 to selcet OTHER. \n";
-			cin >> choiceTeacher;
-			switch (choiceTeacher)
+			do
 			{
-			case 1:
-				subjectTeacher = "math";
-				break;
-			case 2:
-				subjectTeacher = "english";
-				break;
-			case 3:
-				subjectTeacher = "history";
-				break;
-			case 4:
-				subjectTeacher = "science";
-				break;
-			case 5:
-				subjectTeacher = "other";
-				break;
-			}
-			cout << "Select the school location\n";
-			cout << "Choose 1 to select NGS \n"
-				<< "Choose 2 to select CCHS \n"
-				<< "Choose 3 to select CHS \n"
-				<< "Choose 4 to selcet EMS \n"
-				<< "Choose 5 to selcet OTHER. \n";
-			cin >> choiceSchool;
-			switch (choiceSchool)
+				cout << "Select the subject\n";
+				cout << "Choose 1 to select MATH. \n"
+					<< "Choose 2 to select ENGLISH. \n"
+					<< "Choose 3 to select HISTORY. \n"
+					<< "Choose 4 to selcet SCIENCE. \n"
+					<< "Choose 5 to selcet OTHER. \n";
+				cin >> choiceTeacher;
+				switch (choiceTeacher)
+				{
+				case 1:
+					subjectTeacher = "math";
+					break;
+				case 2:
+					subjectTeacher = "english";
+					break;
+				case 3:
+					subjectTeacher = "history";
+					break;
+				case 4:
+					subjectTeacher = "science";
+					break;
+				case 5:
+					subjectTeacher = "other";
+					break;
+				}
+			} while (choiceTeacher > 5);
+			do
 			{
-			case 1:
-				school_location = "NGS";
-				break;
-			case 2:
-				school_location = "CCHS";
-				break;
-			case 3:
-				school_location = "CHS";
-				break;
-			case 4:
-				school_location = "EMS";
-				break;
-			case 5:
-				school_location = "other";
-				break;
-			}
+				cout << "Select the school location\n";
+				cout << "Choose 1 to select NGS \n"
+					<< "Choose 2 to select CCHS \n"
+					<< "Choose 3 to select CHS \n"
+					<< "Choose 4 to selcet EMS \n"
+					<< "Choose 5 to selcet OTHER. \n";
+				cin >> choiceSchool;
+				switch (choiceSchool)
+				{
+				case 1:
+					school_location = "NGS";
+					break;
+				case 2:
+					school_location = "CCHS";
+					break;
+				case 3:
+					school_location = "CHS";
+					break;
+				case 4:
+					school_location = "EMS";
+					break;
+				case 5:
+					school_location = "other";
+					break;
+				}
+			} while (choiceSchool > 5);
 			cout << "Enter room number...";
 			cin >> room_number;
-			cout << "Select the type of day\n";
-			cout << "Enter 1 if it will be a FULL DAY\n"
-				<< "Enter 2 if it will be a HALF DAY\n";
-			cin >> choiceDay;
-			switch (choiceDay)
+			do
 			{
-			case 1:
-				dayType = 420.0;
-				break;
-			case 2:
-				dayType = 210.0;
-				break;
-			}
+				cout << "Select the type of day\n";
+				cout << "Enter 1 if it will be a FULL DAY\n"
+					<< "Enter 2 if it will be a HALF DAY\n";
+				cin >> choiceDay;
+				switch (choiceDay)
+				{
+				case 1:
+					dayType = 420.0;
+					break;
+				case 2:
+					dayType = 210.0;
+					break;
+				}
+			} while (choiceDay > 2);
 			cout << "Enter how many class periods there will be...";
 			cin >> periodNumber;
 			
@@ -189,31 +201,34 @@ int main()
 		cin >> nameSub;
 		cout << "Enter your sub id #";
 		cin >> SubId;
-		cout << "Enter your preferred subject...";
-		cout << "Choose 1 to select MATH. \n"
-			<< "Choose 2 to select ENGLISH. \n"
-			<< "Choose 3 to select HISTORY. \n"
-			<< "Choose 4 to selcet SCIENCE. \n"
-			<< "Choose 5 to selcet OTHER. \n";
-		cin >> choiceSub;
-		switch (choiceSub)
+		do
 		{
-		case 1:
-			subjectSub = "math";
-			break;
-		case 2:
-			subjectSub = "english";
-			break;
-		case 3:
-			subjectSub = "history";
-			break;
-		case 4:
-			subjectSub = "science";
-			break;
-		case 5:
-			subjectSub = "other";
-			break;
-		}
+			cout << "Enter your preferred subject...";
+			cout << "Choose 1 to select MATH. \n"
+				<< "Choose 2 to select ENGLISH. \n"
+				<< "Choose 3 to select HISTORY. \n"
+				<< "Choose 4 to selcet SCIENCE. \n"
+				<< "Choose 5 to selcet OTHER. \n";
+			cin >> choiceSub;
+			switch (choiceSub)
+			{
+			case 1:
+				subjectSub = "math";
+				break;
+			case 2:
+				subjectSub = "english";
+				break;
+			case 3:
+				subjectSub = "history";
+				break;
+			case 4:
+				subjectSub = "science";
+				break;
+			case 5:
+				subjectSub = "other";
+				break;
+			}
+		} while (choiceSub > 5);
 		cout << "Enter email...";
 		cin >> emailSub;
 		cout << nameSub << endl << SubId << endl<< emailSub << endl << "Your perferred subject is " << subjectSub << endl;
